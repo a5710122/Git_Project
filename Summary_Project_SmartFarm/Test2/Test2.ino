a64b46fecaa6bbc253d5ea0edf81dd8f18f478e5
a5710122 <36829216+a5710122@users.noreply.github.com>
2018-07-19 00:06:18 +0700

    Test2.ino Can save data to sd card
    - Test2.ino
    Can save data to sd card

--- Summary_Project_SmartFarm/Test2/Test2.ino
+++ Summary_Project_SmartFarm/Test2/Test2.ino
@@ -1,4 +1,8 @@
+//=================================== LIBRARY ================================//
+
 #include <FlowMeter.h>
+#include <SPI.h>
+#include <SD.h>
 #include <Ultrasonic.h>
 #include <Wire.h>
 #include "Adafruit_MCP23017.h"
@@ -24,7 +28,7 @@ FlowMeter Meter = FlowMeter(2); //Flowmeter D4
 #define MCP_control_pump 8 //Control Pump MCP23017 pin 1 - "8"
 const int switch_stage = 10;   //switch control stage GPIO 10 buttonPin
 const int switch_manual_pump = 9;   //Switch control manual GPIO 9 buttonSW
-const int led = 16;
+File myFile;
 
 //===================================== VARIABLE ================================//
 
@@ -33,6 +37,7 @@ float Flow ; // value flowmeter
 const unsigned long period = 1000; //Constant Flowmeter Interrupt
 unsigned long sensor_millis; //Count time to take value sensor
 unsigned long check_millis; //Count time to take reconnect server
+unsigned long sd_millis; //Count time to save value to sd card
 
 
 long stage = 0; // state button now
@@ -43,29 +48,30 @@ boolean manual_pump = LOW; //Stage pump (manual)
 
 //===================================== MQTT ================================//
 
-const char* ssid = "Maka";
-const char* password =  "";
+const char* ssid = "true_homewifi_99C";
+const char* password =  "00000RY2";
 
-const char* mqttServer = "172.19.201.179";
+const char* mqttServer = "192.168.1.35";
 const int mqttPort = 1883;
 const char* mqttUser = "s5710122";
 const char* mqttPassword = "ap18659993";
 WiFiClient espClient;
 PubSubClient client(espClient);
-long connect_server;
+
 
 //================================== CODE =================================//
 
 void setup() {
   sensor_millis = millis(); //Set time to count for take value
   check_millis = millis();  //Set time to count for reconnect server
+  sd_millis = millis(); //Set time to count for take value
   mcp.begin(); // MCP use default address 0
   Serial.begin (115200);
-  lcd.begin(); //LCD
-  lcd.setCursor(0, 0);
-  lcd.print("SmartFarm");
-  lcd.clear();
-
+  setup_LCD();
+  setup_wifi();
+  setup_MQTT();
+  check_sd();
+  
   pinMode(switch_stage, INPUT); //Call pin in nodemcu input
   pinMode(switch_manual_pump, INPUT);
 
@@ -75,69 +81,56 @@ void setup() {
   attachInterrupt(switch_stage, change_stage, RISING); //interrupt stage
   attachInterrupt(switch_manual_pump, change_stage_manual, RISING); //interrupt pump
   attachInterrupt(2, MeterISR, RISING); //interrupt fan Flowmeter
+
   Meter.reset();
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP8266Client" )) { //Connect to MQTT และกำหนดชื่อในการติดต่อ
-      Serial.println("connected");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-
-    }
-  }
-
-  //ส่งข้อมูล publish ไปที่ MQTT Broker โดยตั้ง topic เป็น "esp/test"
-  client.publish("esp/pump", "Hello from ESP8266");
-
-  //subscribe topic "esp/test"
-  client.subscribe("esp/pump");
-
+  client.publish("esp/pump", "Hello from ESP8266"); //Start massgea if esp connect server
+  client.subscribe("esp/pump"); //subscribe topic "esp/pump"
+  client.subscribe("esp/manual");
 
 }
 
 
 void loop() {
 
+  // Counttime take value and send (publish) to server
   unsigned long now_millis = millis();
   if (now_millis - sensor_millis > 2000) {
-    Read_value_form_sensor();
+    Read_value_form_sensor();  //Read value from sensor
     char buf_ultra[5]; /// array char
-    String valor_ultra = String(Ultra); //// input to string
-    String (valor_ultra).toCharArray(buf_ultra, 5); /// String to array char
-    client.publish("esp/ultra", buf_ultra);
-
+    String valor_ultra = String(Ultra); // input to string ultrasonic
+    String (valor_ultra).toCharArray(buf_ultra, 5); // String to array char
+    client.publish("esp/ultra", buf_ultra); //Publish ultrasonic
     char buf_flow[5]; /// array char
-    String valor_flow = String(Flow); //// input to string
+    String valor_flow = String(Flow); //// input to string Flowmeter
     String (valor_flow).toCharArray(buf_flow, 5); /// String to array char
-    client.publish("esp/flow", buf_flow);
-    sensor_millis = now_millis;
+    client.publish("esp/flow", buf_flow);  //Publish Flowmeter
+    sensor_millis = now_millis; //set time default
   }
 
-  Show_Status_LCD();
-  //Serial.println(stage);
+  unsigned long now_millis_3 = millis(); //Count time
+  if (now_millis_3 - sd_millis > 60 * 1000) { //Countdown time 1 minute
+    check_sd();
+    write_sd();
+    read_sd();
+    sd_millis = now_millis_3;
+  }
 
-  unsigned long now_millis_2 = millis(); //for check
-  if (now_millis - check_millis > 60 * 1000) {
-    if (!client.connected()) {
+  Show_Status_LCD(); //Show value to LCD
+
+  //Counttime to check status server
+  unsigned long now_millis_2 = millis(); //Count time
+  if (now_millis - check_millis > 60 * 1000) { //Countdown time 1 minute
+    if (!client.connected()) { //Check connect
       reconnect();
     }
     check_millis = now_millis_2;
   }
 
   switch (stage) {
+    Switch case
 
-    case 0 : //case 0 auto mode
+      case 0 : //case 0 auto mode
       if (Ultra > 35 or Ultra < 20) { //Auto
         mcp.digitalWrite(MCP_control_pump, LOW); //Auto
         Serial.println("Auto_pump: Status Pump :LOW");
@@ -160,7 +153,11 @@ void loop() {
       show_stage = "manual";
       break;
 
-
+    case 2: //case 1 manual control
+      mcp.digitalWrite(MCP_control_pump, LOW);
+      Serial.println("Stage Off SmartFarm");
+      show_stage = "Stage Off SmartFarm";
+      break;
 
     default:
       if (stage >= 3) {
@@ -216,74 +213,92 @@ void change_stage_manual() {
     manual_pump = !manual_pump;
   }
 
+}
 
+void setup_LCD() {
+  lcd.begin(); //LCD
+  lcd.setCursor(0, 0);
+  lcd.print("SmartFarm");
+  lcd.clear();
+}
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
 
 }
 
+void setup_MQTT() {
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client" )) { //Connect to MQTT และกำหนดชื่อในการติดต่อ
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
 
-void callback(char* topic, byte * payload, unsigned int length) {
+    }
+  }
+}
+void callback(String topic, byte* message, unsigned int length) {
 
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message:");
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
 
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
 
-    //หาก message ที่ส่งกลับมามีค่าเป็น 0 ที่ array index ที่ 10 จะสั่งให้ไฟดับ เช่น {'hello':'0'}
-    //หาก message ที่ส่งกลับมาเป็น 1 จะสั่งให้ไฟติด เช่น {'hello':'1'}
-    //ตรงนี้แล้วแต่เราจะกำหนดครับ แต่ผมอยากทำให้ง่าย ๆ ก่อนเลยใช้วิธีการ fix ค่าไว้ครับ
-
-    if (i == 10) {
-      if ((char)payload[i] == '0') { //turn on/off pump
-        //stage = 1;
-        manual_pump = LOW;
-        //show_stage = "manual";
-      } else {
-        //stage = 1;
-        manual_pump = HIGH;
-        //show_stage = "manual";
-      }
+  if (topic == "esp/manual") {
+    Serial.print("Changing stage to ");
+    if (messageTemp == "0") {
+      stage = 0;
+      Serial.print("0 : Auto mode");
     }
-
-//    if (i == 9) {
-//      if ((char)payload[i] == '0') { //turn on/off manual
-//        stage = 1;
-//        show_stage = "manual";
-//      } else {
-//        stage = 0;
-//        show_stage = "manual";
-//      }
-//    }
-
+    else if (messageTemp == "1") {
+      stage = 1;
+      Serial.print("1 : manual mode");
+    }
   }
 
+  if (topic == "esp/pump") {
+    Serial.print("Changing pump to ");
+    if (messageTemp == "on") {
+      manual_pump = HIGH;
+      Serial.print("On");
+    }
+    else if (messageTemp == "off") {
+      manual_pump = LOW;
+      Serial.print("Off");
+    }
+  }
   Serial.println();
-  Serial.println("-----------------------");
-
 }
+
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    /*
-      YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
-      To change the ESP device ID, you will have to give a new name to the ESP8266.
-      Here's how it looks:
-       if (client.connect("ESP8266Client")) {
-      You can do it like this:
-       if (client.connect("ESP1_Office")) {
-      Then, for the other ESP:
-       if (client.connect("ESP2_Garage")) {
-      That should solve your MQTT multiple connections problem
-    */
+
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
       // Subscribe or resubscribe to a topic
       // You can subscribe to more topics (to control more LEDs in this example)
-      client.subscribe("room/lamp");
+      client.publish("esp/pump", "Hello from ESP8266");
+      client.subscribe("esp/pump");
+      client.subscribe("esp/manual");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -295,5 +310,55 @@ void reconnect() {
   }
 }
 
+void check_sd() {
+  Serial.print("Initializing SD card...");
 
+  if (!SD.begin(SS)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+}
+
+void write_sd() {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("database.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to database.txt...");
+    myFile.print("Flow water: ");
+    myFile.println(Flow);
+    myFile.print("water level: ");
+    myFile.println(Ultra);
+    myFile.println("//======================= done. =====================//");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening database.txt");
+  }
+
+}
+
+void read_sd() {
+  // re-open the file for reading:
+  myFile = SD.open("database.txt");
+  if (myFile) {
+    Serial.println("database.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening database.txt");
+  }
+
+}
 
